d8147b9e3947bc9c5f543f4bc996c1c08bc2a8a4
70NY <53057036+TonyGLL@users.noreply.github.com>
2020-07-27 14:27:52 -0500

    Updates

---
+++ precache-manifest.bcc106b7fa104b03e97d14af2915b8da.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f33f604accce1ed28ab7d3a040ec3ad4",
+    "url": "/tasks-app-reactjs/index.html"
+  },
+  {
+    "revision": "d335090440921bb7085f",
+    "url": "/tasks-app-reactjs/static/css/2.af3c1da9.chunk.css"
+  },
+  {
+    "revision": "d6f4fae41e57f72f8307",
+    "url": "/tasks-app-reactjs/static/css/main.5ecd60fb.chunk.css"
+  },
+  {
+    "revision": "d335090440921bb7085f",
+    "url": "/tasks-app-reactjs/static/js/2.9f51cf69.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/tasks-app-reactjs/static/js/2.9f51cf69.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "d6f4fae41e57f72f8307",
+    "url": "/tasks-app-reactjs/static/js/main.7b99605c.chunk.js"
+  },
+  {
+    "revision": "bf8a6904cf19dbf367fd",
+    "url": "/tasks-app-reactjs/static/js/runtime-main.84d7c776.js"
+  }
+]);
